a0d928b14cdedd82db4f1c30970c85cf53ce18d0
Nataliya Ioffe <nxioffe@gmail.com>
2019-01-18 15:21:02 -0500

    NotFound & Router components added. Capturing input on StorePicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends Component {
+
+  myInput = React.createRef();
+
+  goToStore = (e) => {
+    //1. stop form from submittng
+    e.preventDefault();
+    //2. get text from input
+    const storeName = this.myInput.current.value;
+    //3. change the page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-        <form action="" className="store-selector">
-          <h2>Please Enter A Store</h2>
-          <input type="text" required placeholder="Store Name"/>
-          <button type="submit">Visit Store</button>
-        </form>
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store</button>
+      </form>
     );
   }
 }
